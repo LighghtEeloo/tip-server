d5f398ab324681bf6c25c6310604caf43527ce98
LitiaEeloo <litiaeeloo@gmail.com>
2024-05-23 17:33:56 -0400

    incr: ...

--- src/bin/tip.rs
+++ src/bin/tip.rs
@@ -1,3 +1,26 @@
+const TYPST_SAMPLE: &'static str = r##"
+#set math.equation(numbering: "(1)")
+#{
+  let aba = math.op("aba")
+  [$
+    aba(1)
+  $ <aba1>
+  ]
+};
+
+#let aba = math.op("bab")
+$
+  integral f d aba(2)
+$ <aba2>
+
+$ E = M C ^ 2 $ <einsteins_equation>
+
+$ partial $ <p>
+
+$ multimap $
+
+"##;
+
 fn main() {
-    tip_server::doc_content()
+    tip_server::LabeledTypstWorld::new("./", TYPST_SAMPLE).with("aba1").with("p").render()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,77 +1,88 @@
 mod typst_library;
+
 use comemo::{Track, Tracked, Validate};
+use std::path::PathBuf;
 use typst::diag::SourceResult;
 use typst::engine::{Engine, Route};
 use typst::eval::Tracer;
 use typst::foundations::{Content, Label, Selector, Smart, StyleChain};
 use typst::introspection::{Introspector, Locator};
-use typst::layout::LayoutRoot;
+use typst::layout::{Abs, LayoutRoot};
 use typst::model::Document;
 use typst::World;
 use typst_library::TypstWrapperWorld;
 
-const TYPST_SAMPLE: &'static str = r##"
-#set math.equation(numbering: "(1)")
-#{
-  let aba = math.op("aba")
-  [$
-    aba(1)
-  $ <aba1>
-  ]
-};
+pub struct LabeledTypstWorld {
+    world: TypstWrapperWorld,
+    labels: Vec<String>,
+    dest: PathBuf,
+}
 
-#let aba = math.op("bab")
-$
-  integral f d aba(2)
-$ <aba2>
-
-"##;
-
-pub fn doc_content() {
-    let world = TypstWrapperWorld::new("./".to_string(), TYPST_SAMPLE.to_string());
-    let mut tracer = Tracer::default();
-
-    // original
-    {
-        let document = typst::compile(&world, &mut tracer).expect("Error compiling typst.");
-
-        // Output to pdf and svg
-        let pdf = typst_pdf::pdf(&document, Smart::Auto, None);
-        std::fs::write("_out/original.pdf", pdf).expect("Error writing PDF.");
+impl LabeledTypstWorld {
+    pub fn new(root: impl Into<String>, source: impl Into<String>) -> Self {
+        Self {
+            world: TypstWrapperWorld::new(root.into(), source.into()),
+            labels: Vec::new(),
+            dest: PathBuf::from("_out"),
+        }
     }
 
-    let world = <dyn World as Track>::track(&world);
-    let module = {
-        // Try to evaluate the source file into a module.
-        typst::eval::eval(
-            world,
-            Route::default().track(),
-            tracer.track_mut(),
-            &world.main(),
-        )
-        .unwrap()
-    };
-    let content = module.content();
-
-    // whole
-    {
-        let doc = typeset(world, &mut tracer, &content).unwrap();
-        let pdf = typst_pdf::pdf(&doc, Smart::Auto, None);
-        std::fs::write(format!("_out/whole.pdf",), pdf).expect("Error writing PDF.");
+    pub fn with(mut self, label: impl Into<String>) -> Self {
+        self.labels.push(label.into());
+        self
     }
+    pub fn render(&self) {
+        let world = <dyn World as Track>::track(&self.world);
+        let mut tracer = Tracer::default();
+        let module = {
+            // evaluate the source file into a module
+            typst::eval::eval(
+                world,
+                Route::default().track(),
+                tracer.track_mut(),
+                &world.main(),
+            )
+            .unwrap()
+        };
+        let content = module.content();
 
-    let selector = Selector::Label(Label::new("aba2"));
-    // not an ideal logic
-    let targets = content.query(selector);
-    let docs = targets
-        .into_iter()
-        .map(|tar| typeset(world, &mut tracer, &tar).unwrap())
-        .collect::<Vec<_>>();
+        // whole
+        {
+            let doc = typeset(world, &mut tracer, &content).unwrap();
+            let pdf = typst_pdf::pdf(&doc, Smart::Auto, None);
+            std::fs::write(self.dest.join("whole.pdf"), pdf).expect("Error writing PDF.");
+        }
 
-    for doc in docs {
-        // Output to pdf and svg
-        let pdf = typst_pdf::pdf(&doc, Smart::Auto, None);
-        std::fs::write(format!("_out/labeled.pdf",), pdf).expect("Error writing PDF.");
+        // not an ideal logic
+        let targets = self
+            .labels
+            .iter()
+            .map(|label| {
+                content
+                    .query(Selector::Label(Label::new(label.as_str())))
+                    .into_iter()
+                    .map(|tar| (label.to_owned(), tar))
+            })
+            .flatten()
+            .collect::<Vec<_>>();
+        let docs = targets
+            .into_iter()
+            .map(|(label, tar)| {
+                let doc = typeset(world, &mut tracer, &tar).unwrap();
+                (label, doc)
+            })
+            .collect::<Vec<_>>();
+
+        for (label, doc) in docs {
+            // Output to pdf and svg
+            let pdf = typst_pdf::pdf(&doc, Smart::Auto, None);
+            std::fs::write(self.dest.join(format!("{}.pdf", label)), pdf)
+                .expect("Error writing PDF.");
+
+            let svg = typst_svg::svg_merged(&doc, Abs::pt(2.));
+            std::fs::write(self.dest.join(format!("{}.svg", label)), svg)
+                .expect("Error writing SVG.");
+        }
     }
 }
 
